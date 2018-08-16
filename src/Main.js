4619cd8245212cd115cdb439ba0288e910168cb0
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-08-16 20:04:44 +0900

    Create Main.js

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <div className="Calendar-header">
+          <div className="Calendar-header-text">xx月</div>
+        </div>
+        <div className="Calendar-body">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
