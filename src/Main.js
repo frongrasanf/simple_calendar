78af41ce25a4be9a7ee5b8d1d4b72c8bbf79f795
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-08-16 22:00:24 +0900

    Create meveToNextMonth function

--- src/Main.js
+++ src/Main.js
@@ -15,9 +15,68 @@ class Main extends Component {
       month: month,
       year: year,
       firstDay: firstDay,
-      displayDays: []
+      displayDays: [],
+      newScheduleTitle: ""
+    }
+    this.moveToNextMonth = this.moveToNextMonth.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(e) {
+    this.setState({newScheduleTitle: e.target.value});
+  }
+  handleSubmit() {
+  }
+  moveToNextMonth() {
+  const { firstDay, month, displayDays, year } = this.state
+  let tsugi = month + 1
+  let tempYear = year
+  let firstDay2 = ""
+  // 12月から1月の場合
+  if (tsugi == 12) {
+    tsugi = 0
+    tempYear = tempYear + 1
+    firstDay2 = moment().year(tempYear).month(tsugi).date(1)
+  } else {
+    firstDay2 = moment().year(tempYear).month(tsugi).date(1)
+  }
+
+  //ここから表示する日付を計算
+  let tempDay = firstDay2;
+  let wday = firstDay2.day();
+  let tempMonth = tsugi
+
+  let dayArray = [];
+  //1日よりも前の日を入れる
+  for (let i = 0; i < wday; i++) {
+    let beforeDay = tempDay.subtract(wday - i, 'days').format("YYYY/M/D")
+    dayArray.push(beforeDay)
+    tempDay.add(wday - i, 'days').format("YYYY/M/D")
+  }
+
+  let flag = true
+  while(flag = true) {
+    let day = tempDay.format("YYYY/M/D")
+    dayArray.push(day)
+    tempDay.add(1, 'days').format("YYYY/M/D")
+    // 12月を表示する場合、ここは11
+    let tsugi2 = tempDay.month()
+    if (tempDay.day() === 0) {
+      if (tsugi2 === tsugi + 1) {
+        flag = false
+        break;
+      } else if (tsugi === 11 && tsugi2 === 0) {
+        flag = false
+        break;
+      }
     }
   }
+  this.setState({
+    month: tsugi,
+    year: tempYear,
+    displayDays: dayArray
+  })
+}
 
   componentWillMount() {
     console.log("will")
@@ -29,17 +88,17 @@ class Main extends Component {
     let dayArray = [];
     // 1日よりも前の日を入れる
     for (let i = 0; i < wday; i++) {
-      let beforeDay = tempDay.subtract(wday - i, 'days').format("D")
+      let beforeDay = tempDay.subtract(wday - i, 'days').format("YYYY/M/D")
       dayArray.push(beforeDay)
-      tempDay.add(wday - i, 'days').format("D")
+      tempDay.add(wday - i, 'days').format("YYYY/M/D")
     }
 
     let nextMonth = tempMonth + 1
     let flag = true
     while(flag = true) {
-      let day = tempDay.format("D")
+      let day = tempDay.format("YYYY/M/D")
       dayArray.push(day)
-      tempDay.add(1, 'days').format("D")
+      tempDay.add(1, 'days').format("YYYY/M/D")
       tempMonth = tempDay.month()
       if (tempDay.day() === 0) {
         if (tempMonth === nextMonth) {
@@ -53,7 +112,6 @@ class Main extends Component {
     })
   }
   render() {
-    console.log("state", this.state)
     const weekInfo = (
       <div className="Week-info">
         <div className="Week-row">
@@ -71,13 +129,13 @@ class Main extends Component {
     return (
       <div>
         <div className="Calendar-header">
-          <button onClick={this.moveToNextMonth}>先月</button>
-          <button onClick={this.moveToNextMonth}>次月</button>
-          <div className="Calendar-header-text">xx月</div>
+          <button className="Month-button">先月</button>
+          <button className="Month-button" onClick={this.moveToNextMonth}>次月</button>
+          <div className="Calendar-header-text">{this.state.year}年 {this.state.month + 1}月</div>
         </div>
         <div className="Calendar-body">
           {weekInfo}
-          <DisplayWeeks displayDays={this.state.displayDays} />
+          <DisplayWeeks moveToNextMonth={this.moveToNextMonth} displayDays={this.state.displayDays} />
         </div>
       </div>
     );
