629b10b19b1cd3e2f4abc26bf94ac05161167fea
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-08-16 20:15:05 +0900

    Create weekInfo component

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,27 @@ import './Main.css';
 
 class Main extends Component {
   render() {
+    const weekInfo = (
+      <div className="Week-info">
+        <div className="Week-row">
+          <div className="Day-box Sunday">Sun</div>
+          <div className="Day-box">Mon</div>
+          <div className="Day-box">Tue</div>
+          <div className="Day-box">Wed</div>
+          <div className="Day-box">Thu</div>
+          <div className="Day-box">Fri</div>
+          <div className="Day-box Saturday">Sat</div>
+        </div>
+      </div>
+    )
+
     return (
       <div>
         <div className="Calendar-header">
           <div className="Calendar-header-text">xx月</div>
         </div>
         <div className="Calendar-body">
+          {weekInfo}
         </div>
       </div>
     );
